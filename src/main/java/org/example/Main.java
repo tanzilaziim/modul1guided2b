b7d0dd664916940f93912f43a7420a634374afcc
tanzilaziim <tanzilrxbow@gmail.com>
2023-10-07 21:23:59 +0700

    Guided 2b modul 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String args[])
+    {
+        Mahasiswa M=new Mahasiswa();
+        M.inputData();
+        M.display ();
+    }
+}
